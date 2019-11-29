814ad94e2477c2bb9fdf699a17b5f49ca07c4c32
Yogesh Desai <er.yogeshdesai@gmail.com>
2019-11-29 21:26:43 +0530

    custom actions

---
+++ .github/golangci/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -e
+
+APP_DIR="$GOPATH/src/github.com/${GITHUB_REPOSITORY}"
+
+mkdir -p ${APP_DIR}
+cp -r ./ ${APP_DIR} && cd ${APP_DIR}
+
+
+echo "build"
+make build-all
+
+echo "Run Golangci-lint"
+golangci-lint run ./... --skip-dirs=vendor
+
+echo "Go Test"
+go test -v -cover -race ./...
