fade8ebd07c524bf7821913b3e3ad0fd4fec8a52
Yogesh Desai <er.yogeshdesai@gmail.com>
2019-11-29 20:25:18 +0530

    updated sample

--- main_test.go
+++ main_test.go
@@ -2,6 +2,9 @@ package main
 
 import "testing"
 
-func TestMain(*testing.T) {
-
+func TestMain(t *testing.T) {
+	x := add(5, 3)
+	if x != int(8) {
+		t.Errorf("Wrong output. Expected: 8 but got: %v", x)
+	}
 }
