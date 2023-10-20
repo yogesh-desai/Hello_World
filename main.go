54689023995960bab265ac01ae04d6b16973d017
Yogesh Desai <er.yogeshdesai@gmail.com>
2023-10-21 00:23:26 +0530

    Improved the test coverage printHello (#13)

--- main.go
+++ main.go
@@ -5,5 +5,13 @@ import (
 )
 
 func main() {
+	err := printHelloWorld("Hello World.")
+	if err != nil {
+		fmt.Println("[main] error in printing.")
+	}
+}
+
+func printHelloWorld(input string) (err error) {
 	fmt.Println("Hello World")
+	return err
 }
