e47d01661355e4776bf447a04b291edb3b287d21
Yogesh Desai <er.yogeshdesai@gmail.com>
2023-10-21 00:04:56 +0530

    Improved the test coverage printHello

--- main_test.go
+++ main_test.go
@@ -1,7 +1,46 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
-func TestMain(*testing.T) {
+func Test_printHelloWorld(t *testing.T) {
+	type args struct {
+		input string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "simple test",
+			args: args{
+				input: "Hello",
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := printHelloWorld(tt.args.input); (err != nil) != tt.wantErr {
+				t.Errorf("printHelloWorld() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
 
+func Test_main(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		{
+			name: "Hello",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			main()
+		})
+	}
 }
